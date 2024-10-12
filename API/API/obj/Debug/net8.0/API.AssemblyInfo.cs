945cce39ebb09d7752ed4d207ff9f8ecf011698b
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-10-11 21:05:04 -0600

    Rename en el archivo de ProductoController

--- API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e69f8bbd278c304a6e9f9e3505124bf96252b3c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+865f0ff3e15dc6feb0ed033f5e808409b584a12e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
