f686e0bc9b445680e650a4153b0300c0393e40e4
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-10-11 19:21:17 -0600

    Creación de variables

--- API/API/Controllers/Producto.cs
+++ API/API/Controllers/Producto.cs
@@ -6,12 +6,18 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class Producto : ControllerBase
     {
+        // Variables
+        private static List<string> _productos = new List<string>
+        {
+            "TV", "Itachi", "Venom", "Laptop", "Monitor"
+        };
+
         [HttpGet]
         public IActionResult Index()
         {
-            return Ok("Saludos clase");
+            return Ok(_productos);
         }
-        //GET, POST, PUT, DELETE CRUD
+        // GET, POST, PUT, DELETE CRUD
 
     }
 }
