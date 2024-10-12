10d009b505b4c6b9d1911f204ca88c021e01965a
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-10-11 20:39:53 -0600

    Función de eliminar productos

--- API/API/Controllers/Producto.cs
+++ API/API/Controllers/Producto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
 
 namespace API.Controllers
 {
@@ -12,39 +13,58 @@ namespace API.Controllers
             "TV", "Itachi", "Venom", "Laptop", "Monitor"
         };
 
+        /// <summary>
+        /// Métodos CRUD - GET, POST, PUT, DELETE
+        /// </summary>
+        /// <returns></returns>
+
+        // GET
         [HttpGet]
         public IActionResult obtenerProductos()
         {
             return Ok(_productos);
         }
 
+        // GET by id
         [HttpGet("{id}")]
         public IActionResult obtenerProductos(int id)
         {
             if (id >= _productos.Count || id <= 0)
             {
-                return NotFound("El ID no está en el sistema. ");
+                return NotFound("El id no está en el sistema. ");
             }
 
             return Ok(_productos[id]);
         }
-        // GET
 
-        // GET, POST, PUT, DELETE CRUD
+        // POST
         [HttpPost]
         public IActionResult agregarProductos([FromBody] string _nuevoProducto)
         {
-            if (string.IsNullOrWhiteSpace(_nuevoProducto) || string.IsNullOrEmpty(_nuevoProducto)
-            { 
-                return BadRequest("El nombre no es válido. ")
+            if (string.IsNullOrWhiteSpace(_nuevoProducto) || string.IsNullOrEmpty(_nuevoProducto))
+            {
+                return BadRequest("El nombre no es válido. ");
             }
 
             _productos.Add(_nuevoProducto);
             return CreatedAtAction(
-                nameof(obtenerProductos), 
+                nameof(obtenerProductos),
                 new { id = _productos.Count - 1 }
-                );
+            );
         }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public IActionResult borrarProductos(int id)
+        {
+            return NotFound("El id no está en el sistema. ");
+            _productos.RemoveAt(id);
+
+            return NoContent();
+        }
+
+        // PUT
+
 
     }
 }
