b321f45a903df579a14224150bd45ec0793086c5
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-10-18 21:44:50 -0600

    Ajustes en los archivos

--- API/API/Data/Minombredeconexion.cs
+++ API/API/Data/Minombredeconexion.cs
@@ -2,7 +2,8 @@
 
 namespace API.Data
 {
-    public class Minombredeconexion : DbContext
+    //Heredar de la base entityFramework, creamos el contexto de la base de datos
+    public class MinombredeconexionDbContext : DbContext
     {
         public MinombredeconexionDbContext(DbContextOptions<MinombredeconexionDbContext> options)
             : base(options) { }
@@ -12,5 +13,8 @@ namespace API.Data
         //"ConnectionString": {
         //  "Minombredeconexion": "Server=srv863.hstgr.io;Port=3306;User=u484426513_pac324;Password=B&XWouC#9Ef;Database=u484426513_pac324;"
         //},
+
+
+
     }
 }
--- API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7216a99d13375bb20a4195dec70be9a1cb175caa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7072ae9fcc1a4caaa1f1f39aab846d4ea739437")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
