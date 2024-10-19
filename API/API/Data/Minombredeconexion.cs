e7072ae9fcc1a4caaa1f1f39aab846d4ea739437
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-10-18 21:41:32 -0600

    Proceso para realizar una conexión a base de datos

---
+++ API/API/Data/Minombredeconexion.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class Minombredeconexion : DbContext
+    {
+        public MinombredeconexionDbContext(DbContextOptions<MinombredeconexionDbContext> options)
+            : base(options) { }
+
+
+
+        //"ConnectionString": {
+        //  "Minombredeconexion": "Server=srv863.hstgr.io;Port=3306;User=u484426513_pac324;Password=B&XWouC#9Ef;Database=u484426513_pac324;"
+        //},
+    }
+}
--- API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ef81a3cd587a5334980c6fd83a3e41af538a063")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7216a99d13375bb20a4195dec70be9a1cb175caa")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
