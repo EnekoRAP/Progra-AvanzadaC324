69e33801b1cdf1a807bc9f00c4de26853500e756
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-11-22 19:24:00 -0600

    Create UserModel.cs

---
+++ Laboratorio_Autenticacion/Pages/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Laboratorio_Autenticacion.Pages.Models
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        // Ejempko de JSON
+
+        // {
+        // "name": "nombre_usuario",
+        // "password": "contraseña",
+        // "email": "correo@ejemplo.com"
+        // }
+    }
+}
