02b430c6e159d3c0744dce1b4b60f70b976ea80c
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-11-22 18:52:17 -0600

    Vista de Registro

--- Laboratorio_Autenticacion/Pages/Usuarios/Registro.cshtml.cs
+++ Laboratorio_Autenticacion/Pages/Usuarios/Registro.cshtml.cs
@@ -1,3 +1,5 @@
+using Laboratorio_Autenticacion.Pages.Models;
+using Laboratorio_Autenticacion.Pages.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,33 @@ namespace Laboratorio_Autenticacion.Pages.Usuarios
 {
     public class RegistroModel : PageModel
     {
-        public void OnGet()
+        private readonly ApiService _ppiService;
+
+        public RegistroModel(ApiService ppiService)
         {
+            _ppiService = ppiService;
+        }
+
+        // Las propiedades o los binds, estos serán mensaje de intercambio hacia el usuario
+        [BindProperty]
+        public UserModel User { get; set; }
+
+        public string Mensaje { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var resultado = await _ppiService.RegisterUserAsync(User);
+
+            // if ternario
+
+            Mensaje = resultado ? "Se ingresó los datos " : "No se ingresó los datos ";
+
+            return Page();
         }
     }
 }
