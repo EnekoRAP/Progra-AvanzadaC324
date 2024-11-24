2440a82b5bbb141a097a384933d6e4f21cfdba1e
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-11-23 22:09:08 -0600

    Autenticacion y Password model

---
+++ Laboratorio_Autenticacion/Pages/Usuarios/Autenticacion.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Laboratorio_Autenticacion.Pages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class AutenticacionModel : PageModel
+{
+    [BindProperty]
+    public UserModel usuario { get; set; }
+    public string mensaje { get; set; }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var url = "https://paginas-web-cr.com/Api/apis/AutenticarUsuario.php";
+        var carga = new { email = usuario.Email, password = usuario.Password };
+        var json = JsonSerializer.Serialize(carga);
+
+        using var client = new HttpClient();
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var respuesta = await client.PostAsync(url, content);
+        var responseContent = await respuesta.Content.ReadAsStringAsync();
+
+        mensaje = respuesta.IsSuccessStatusCode && responseContent.Contains("True")
+            ? "Credenciales válidas. Inicio de sesión exitoso."
+            : "Credenciales inválidas. Intenta nuevamente.";
+
+        return Page();
+    }
+}
---
+++ Laboratorio_Autenticacion/Pages/Usuarios/ReenviarPassword.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Laboratorio_Autenticacion.Pages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class ReenviarPasswordModel : PageModel
+{
+    [BindProperty]
+    public UserModel usuario { get; set; }
+    public string mensaje { get; set; }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var url = "https://paginas-web-cr.com/Api/apis/SendPassword.php";
+        var carga = new { email = usuario.Email };
+        var json = JsonSerializer.Serialize(carga);
+
+        using var client = new HttpClient();
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var respuesta = await client.PostAsJsonAsync(url, content);
+
+        mensaje = respuesta.IsSuccessStatusCode
+            ? "Correo de recuperación enviado exitosamente."
+            : "Hubo un problema al enviar el correo.";
+
+        return Page();
+    }
+}
+
